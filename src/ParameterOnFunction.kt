d3b42c18d3e5beaee331dcf36615e37df4a6d074
bootgem2 <bootgem2@gmail.com>
2019-12-16 15:00:44 +0700

    learn added parameter on function and how to use 2 parameter on a function

---
+++ src/ParameterOnFunction.kt
@@ -0,0 +1,18 @@
+// here we will study how to use parameter on kotlin
+fun sayHello(itemToGreet: String){
+//    val msg = "Helo "+ itemToGreet
+    // here we can see that the kotlin suggest use to change that String into template
+    // so lets learn how to do it
+    val msg ="Hello $itemToGreet"
+    // and we will found that the out put same with before :)
+    println(msg)
+    // u can modify it like any type function do you want
+}
+
+fun greeting(greet: String, itemToGreet: String) = println("$greet $itemToGreet")
+
+fun main() {
+    sayHello("Kotlin")
+    sayHello("World! ")
+    greeting("Hey", "Kotlin")
+}
