594f139525ebaeee255b6a799fe2ab9e6a8d6639
bootgem2 <bootgem2@gmail.com>
2019-12-16 15:18:13 +0700

    Learn about Collection and Iteration on Kotlin

---
+++ src/CollectionAndIteration_4.kt
@@ -0,0 +1,12 @@
+//Kotlin is like most programing language
+//he has collection like array, list and maps and it can use to group values
+// together and then operate them later time
+
+fun main() {
+    val interstingThings = arrayOf("C","C++","Java","Flutter","Kotlin")
+    println(interstingThings.size)
+    println(interstingThings[4]) // get the value of array by indexes
+    println(interstingThings.get(0))//get value of array by indexes
+
+    
+}
