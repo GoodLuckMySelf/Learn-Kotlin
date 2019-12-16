7e831702b2a6e3502789d54d0beb872953a2d6eb
bootgem2 <bootgem2@gmail.com>
2019-12-16 14:10:22 +0700

    using if and when statement to cange the value of some variable, it depending on that input value that its checking

--- src/Main.kt
+++ src/Main.kt
@@ -6,17 +6,17 @@
 // and on kotlin variable can we declaration on outside of a function/method
 // and here we don't need semicolon. bye bye semicolon :v
 
-val greeting: String="Hello World!. Lets Learn Kotlin Language" // this variable is a top level variable
+    var greeting: String? = null // this variable is a top level variable
 // top level variable will work like a local variable
 
 // the difference kotlin and java is, the type in kotlin are a non null by default
 //so if we have a some variable that contain null value so we have add a question mark on that variable
 // so what it is look like? lets check it out
-var nick: String? = null
+    var nick: String? = null
 // and on kotlin it can detect what type of variable it is, like this example
-var num = 1.4
+    val num = 1.4
 // so if we want to add null value for variable with unknown type we must declaration null value first
-var varnum = null
+    var varnum = null
 fun main() {
     nick="azizi"
 // creating if else statement
@@ -33,6 +33,21 @@ fun main() {
         null -> println("Nick is null")
         else -> println("nick has value "+nick)
     }
+
+//    using if and when statement on a different action on kotlin
+//    if and when can also we used as expression to assign a value to a local variable
+//    this the example when we use if statement
+    val greetingToPrint = if(greeting != null) greeting else "Hi"
+    println(greetingToPrint)
+//    this when we use when statement
+    greeting = "Hello World!. Lets Learn Kotlin Language"
+    val greetingToPrint1 = when(greeting){
+        null -> "value is null"
+        else -> greeting
+    }
+    println(greetingToPrint1)
+
+
 
 
 }
